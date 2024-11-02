c2bdb23a20541390e64be92dc4f7ed734002070e
Kritidipto Ghosh <kritidipto.ghosh@wingify.com>
2024-11-03 01:27:20 +0530

    style(pickers): further styling

--- src/components/DateRangePicker.tsx
+++ src/components/DateRangePicker.tsx
@@ -123,7 +123,9 @@ function DateRangePicker({startDate, endDate, setStartDate, setEndDate, config}:
     };
 
     const dateRangePickerJsx =  <div className={styles.DateRangePicker}>
-        <div>No of days selected: {(internalEndDate.getTime() - internalStartDate.getTime()) / (1000 * 60 * 60 * 24) +1}</div>
+        <div className={styles.daysSelected}>
+            No of days selected: <span>{(internalEndDate.getTime() - internalStartDate.getTime()) / (1000 * 60 * 60 * 24) + 1}</span>
+        </div>
         <div className={styles.DatePickerContainer}>
             <DatePicker 
                 {...commonProps}
@@ -160,8 +162,16 @@ function DateRangePicker({startDate, endDate, setStartDate, setEndDate, config}:
             if(!v) resetPicker();
         return !v});
     }
-    return <div >
-        <button onClick={toggleVisibility}>Range Picker</button>
+    return <div className={styles.container}>
+        <button className={styles.rangePicker} onClick={toggleVisibility}>
+            <span>Range Picker</span>
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M16 2V6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M8 2V6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M3 10H21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+        </button>
         {visible && <Modal closeModal={closeDatePickerModal}>{dateRangePickerJsx}</Modal>}
     </div>
 }

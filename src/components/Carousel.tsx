02aba8e406d43f550ca117804c1c3058dbd30660
Kritidipto Ghosh <kritidipto.ghosh@wingify.com>
2024-12-25 21:01:41 +0530

    feat(): added basic functionality

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,9 +1,22 @@
 import classes from './Carousel.module.scss';   
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Carousel = ({images}: {images: string[]}) => {
 
     const [currentImage, setCurrentImage] = useState(0);
+    const containerRef = useRef<HTMLDivElement>(null);
+    const imagesRef = useRef<HTMLImageElement[]>([]);
+
+    useEffect(() => {
+        let scrollX = 0;
+        imagesRef.current.forEach((image, index) => {
+            if (index === currentImage) {
+                scrollX = image.getBoundingClientRect().left-containerRef.current!.getBoundingClientRect().left + containerRef.current!.scrollLeft;
+            }
+        });
+
+        containerRef.current?.scrollTo({left: scrollX, behavior: 'smooth'});
+    }, [currentImage]);
 
     const handleNext = () => {
         setCurrentImage(i => (i+1)%images.length);
@@ -16,11 +29,12 @@ const Carousel = ({images}: {images: string[]}) => {
     return (
         <div className={classes.Carousel}>
             <div className={classes.Carousel__arrow} onClick={handlePrevious}>{'<'}</div>
-            <div className={classes.Carousel__images}>
+            <div ref={containerRef} className={classes.Carousel__images}>
                 {images.map((image, index) => (
-                    <img className={classes.Carousel__image + (index === currentImage ? ' ' + classes.Carousel__image__active : '')} src={image} alt={`Image ${index}`} key={index} />
+                    <img ref={el => imagesRef.current[index] = el!} className={classes.Carousel__image + (index === currentImage ? ' ' + classes.Carousel__image__active : '')} src={image} alt={`Image ${index}`} key={index} />
                 ))}
             </div>
+
             <div className={classes.Carousel__arrow} onClick={handleNext}>{'>'}</div>
         </div>
     )

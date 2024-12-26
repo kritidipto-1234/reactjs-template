0e967b566b78c79691324868f90d03b5610e676a
Kritidipto Ghosh <kritidipto.ghosh@wingify.com>
2024-12-26 16:48:02 +0530

    feat(): changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Carousel from './components/Carousel.tsx';
 
 
 const images = [
-  'https://cdn.pixabay.com/photo/2012/04/23/15/20/one-38484_1280.png',
+  'https://cdn.pixabay.com/photo/2012/04/23/15/20/one-38484_1280.png ',
   'https://cdn.pixabay.com/photo/2012/04/23/17/07/two-39115_1280.png',
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdDuTY_wiWhRrC8xCOwkzxLgZn6b4FkeJFHg&s',
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBOXQ_uEkrVx6HVxTSA3q95DDXQpKJyPKk6A&s',
@@ -17,7 +17,7 @@ const App: React.FC = () => {
     <>
       <Carousel >
         {images.map((image, index) => (
-          <img src={image} alt={`Image ${index}`} key={index} />
+          <img width={200} src={image} alt={`Image ${index}`} key={index} />
         ))}
       </Carousel>
     </>
